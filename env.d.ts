de080734c4daa440c1286c38bde242a3bc2cf59b
hanosong <838519800@qq.com>
2024-03-02 15:18:13 +0800

    初始化项目

---
+++ env.d.ts
@@ -0,0 +1,8 @@
+// ts的引用声明类型
+/// <reference types="vite/client" />
+
+// declare module '*.vue' {
+//   import { DefineComponent } from 'vue'
+//   const component: DefineComponent
+//   export default component
+// }
