0f228a56feca925f73a91dd07949a7f504ff3f9b
hanosong <838519800@qq.com>
2024-03-02 16:39:00 +0800

    解决一些vscode报错和配置网页标题

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,12 @@
 // ts的引用声明类型
-/// <reference types="vite/client" />
+// 引入.jpg ... 文件类型打包不报错
+/// <reference types="vite/client" /> 
 
-// declare module '*.vue' {
-//   import { DefineComponent } from 'vue'
-//   const component: DefineComponent
-//   export default component
-// }
+// 引入.vue 文件类型打包不报错 => 使用了volar可以解决报错，但是不使用插件的话需要如下配置
+// 声明引入的.vue 文件都是一个个组件类型
+/* declare module '*.vue' {
+  import { DefineComponent } from 'vue' // DefineComponent 类型，
+  const component: DefineComponent
+  export default component
+}
+ */
