956ae0545386e28638dd9e8a6ad1c748cdada306
hanosong <838519800@qq.com>
2024-03-05 13:38:12 +0800

    登录的时候加入类型校验

--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -1,15 +1,17 @@
 import {defineStore} from "pinia"
 import accountLoginRequest from "@/service/login/login"
+import type { IAccount } from '@/types'
 import { localCache } from "@/utils/cache";
 
+const LOGIN_TOKEN = 'token'
 const useLoginStore = defineStore('login', {
   state: () =>({
     id: '',
-    token: localCache.getCache('token') ?? '',
+    token: localCache.getCache(LOGIN_TOKEN) ?? '',
     name: '',
   }),
   actions: {
-    async loginAccountAction(account: {name: string, password: string}){
+    async loginAccountAction(account: IAccount){
       const loginRes = await accountLoginRequest(account)
       // 保存在pinia中的数据是在内存中的，还需要做持久化，防止用户刷新
       this.id = loginRes.data.id;
@@ -18,7 +20,7 @@ const useLoginStore = defineStore('login', {
 
       // 进行本地缓存
       // localStorage.setItem('token', this.token);
-      localCache.setCache('token', this.token)
+      localCache.setCache(LOGIN_TOKEN, this.token)
     }
   }
 })
