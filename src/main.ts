1c34ee690fa32fe26b2987ab4aee3c7c7c9de1b8
hanosong <838519800@qq.com>
2024-03-06 16:13:02 +0800

    优化：刷新页面的时候路由持久化

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import pinia from './store'
+import store from './store'
 
 import 'normalize.css'
 import './assets/css/index.less' // vite 默认支持less，需要安装
@@ -9,12 +9,8 @@ import 'element-plus/dist/index.css'
 // import "element-plus/theme-chalk/el-message.css"; // 引入message样式
 import registerIcons from './global/register-icons'
 
-import useLoginStore from './store/login/login'
-
 const app = createApp(App)
 app.use(registerIcons) // use 一个函数， 函数会自动调用
-app.use(pinia)
-const loginStore = useLoginStore()
-loginStore.loadLocalCacheAction()
+app.use(store)
 app.use(router) // 最后再加载路由
 app.mount('#app')
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,12 @@
 import { createPinia } from 'pinia'
-
+import useLoginStore from './login/login'
 const pinia = createPinia()
+const registerStore = (app: App<Element>) => {
+  // 1. use pinia
+  app.use(pinia)
+  // 2. 加载本地数据
+  const loginStore = useLoginStore()
+  loginStore.loadLocalCacheAction()
+}
 
-export default pinia
+export default registerStore
