5c38beff39d09e1ddade3145856a64ac2f0ddcef
hanosong <838519800@qq.com>
2024-03-27 20:27:58 +0800

    对hook进行注释

--- src/hooks/usePageContent.ts
+++ src/hooks/usePageContent.ts
@@ -3,9 +3,11 @@ import type PageContent from "@/components/page-content/page-content.vue" // 注
 export const usePageContent = () => {
   // 点击search, content的操作
   const contentRef = ref<InstanceType<typeof PageContent>>()
+  // 查询
   const handleQueryClick = (queryInfo: any) => {
     contentRef.value?.fetchPageListData(queryInfo) // 让内容组件进行搜索
   }
+  // 重置
   const handleResetClick = () => {
     contentRef.value?.fetchPageListData()
   }
--- src/hooks/usePageModal.ts
+++ src/hooks/usePageModal.ts
@@ -2,9 +2,11 @@ import { ref } from "vue"
 import type pageModal from "@/components/page-modal/page-modal.vue"
 export const usePageModal = () => {
   const modalRef = ref<InstanceType<typeof pageModal>>()
+  // 新建
   const handleNewClick = () => {
     modalRef.value?.setModalVisible()
   };
+  // 编辑
   const handleEditClick = (itemData: any) => {
     modalRef.value?.setModalVisible(false, itemData)
   };
