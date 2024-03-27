84525ba2b3a480a3ba73cfa714dce800e8e123c5
hanosong <838519800@qq.com>
2024-03-27 21:27:54 +0800

    菜单页面搭建（除了树形表格展开部分）

---
+++ src/views/main/system/menu/config/contentConfig.ts
@@ -0,0 +1,24 @@
+const contentConfig = {
+  pageName: "menu",
+  header: {
+    title: "菜单列表",
+    btnTitle: "新建菜单",
+  },
+  // 表格的字段
+  propsList: [
+    { type: 'index', label: '序号', width: '80px' },
+    { type: 'normal', label: '菜单名称', prop: 'name', width: '150px' },
+    { type: 'normal', label: '菜单级别', prop: 'type', width: '150px' },
+    { type: 'normal', label: '菜单url', prop: 'url', width: '150px' },
+    { type: 'normal', label: '菜单图标', prop: 'icon', width: '200px' },
+    { type: 'normal', label: '排序', prop: 'sort', width: '150px' },
+    { type: 'normal', label: '权限', prop: 'permission', width: '150px' },
+    // 日期相关
+    { type: 'timer', label: '创建时间', prop: 'createAt' },
+    { type: 'timer', label: '更新时间', prop: 'updateAt' },
+    // 按钮操作相关
+    { type: 'handler', label: '操作', width: '150px' }
+  ]
+}
+
+export default contentConfig
--- src/views/main/system/menu/menu.vue
+++ src/views/main/system/menu/menu.vue
@@ -1,10 +1,15 @@
 <template>
+  <!-- 菜单管理 -->
   <div class="menu">
-    <h2>menu</h2>
+
+    <page-content :content-config="contentConfig"></page-content>
   </div>
 </template>
 
-<script setup lang="ts" name="menu"></script>
+<script setup lang="ts" name="menu">
+import pageContent from '@/components/page-content/page-content.vue';
+import contentConfig from "./config/contentConfig.ts";
+</script>
 
 <style scoped>
 .menu {
