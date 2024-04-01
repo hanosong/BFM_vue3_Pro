66a3320a10b45473e19e7e6f37d514b6b33a70b7
hanosong <838519800@qq.com>
2024-04-01 10:40:37 +0800

    获取商品数据的接口

--- src/store/analysis/analysis.ts
+++ src/store/analysis/analysis.ts
@@ -1,21 +1,29 @@
 import { defineStore } from 'pinia'
-import { getAmountList } from '@/service/main/analysis/analysis'
+import { getAmountList, getGoodsCategoryCount } from '@/service/main/analysis/analysis'
 
 interface IAnalysisState {
   amountList: any[]
+  goodsCategoryCount: any[]
 }
 
 const useAnalysisStore = defineStore('analysis', {
   state: (): IAnalysisState => {
     return {
-      amountList: []
+      amountList: [],
+      goodsCategoryCount: [],
     }
   },
   actions: {
-    async fetchAnalysisDataAction() {
-      const res = await getAmountList()
-      console.log(res.data, "res.data")
-      this.amountList = res.data
+    fetchAnalysisDataAction() {
+      // 以下数据接口均没有相互依赖关系，所以不用await
+      getAmountList().then((res) => {
+        console.log(res.data, "res.data")
+        this.amountList = res.data
+      })
+
+      getGoodsCategoryCount().then(res => {
+        this.goodsCategoryCount = res.data
+      });
     }
   }
 })
