ec52ebf76b040f85a46ecdba03ede5878475ca93
hanosong <838519800@qq.com>
2024-04-01 13:04:42 +0800

    调整echarts的大小样式

--- src/views/main/analysis/dashboard/c-cpns/chart-card/chart-card.vue
+++ src/views/main/analysis/dashboard/c-cpns/chart-card/chart-card.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="chart-card">
-    <el-card style="max-width: 480px" :header="header">
+    <el-card :header="header">
       <!-- 预留插槽-展示echats -->
       <slot>默认的内容</slot>
     </el-card>
