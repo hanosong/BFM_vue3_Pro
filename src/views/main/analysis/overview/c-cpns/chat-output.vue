7c6fd30f2b2e3caa5405bdb4a70c17600e26a7f2
hanosong <838519800@qq.com>
2024-04-02 13:54:08 +0800

    核心技术页面搭建，仿造gpt光标效果

---
+++ src/views/main/analysis/overview/c-cpns/chat-output.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="result-streaming">
+    <slot></slot>
+    <span v-if="showCursor" class="cursor"></span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref, onMounted} from 'vue';
+const showCursor = ref(false);
+
+onMounted(() => {
+  setInterval(() => {
+    showCursor.value = !showCursor.value;
+  }, 500); // 光标闪烁的间隔时间（单位：毫秒）
+});
+
+
+
+
+</script>
+
+
+<style>
+.result-streaming>:not(ol):not(ul):not(pre):last-child:after,
+.result-streaming>ol:last-child li:last-child:after,
+.result-streaming>pre:last-child code:after,
+.result-streaming>ul:last-child li:last-child:after {
+  -webkit-animation: blink .1s steps(2, start) infinite;
+  animation: blink .1s steps(2, start) infinite;
+  content: "";
+  /* margin-left: 0.25rem; */
+  vertical-align: baseline;
+}
+.result-streaming{
+  display: flex;
+}
+.cursor {
+  display: inline-block;
+  width: 0.2rem; /* 光标宽度 */
+  height: 1rem; /* 光标高度 */
+  margin-left: 2px;
+  background-color: #0A60BD; /* 光标颜色 */
+  animation: blink 1s infinite;
+}
+
+@keyframes blink {
+  0%,
+  100% {
+    opacity: 0; /* 光标透明度 */
+  }
+  50% {
+    opacity: 1;
+  }
+}
+</style>onMounted,
--- src/views/main/analysis/overview/overview.vue
+++ src/views/main/analysis/overview/overview.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="overview">
-    <h2>overview</h2>
+    <chat-output>
+        <div>{{ text }}</div>
+    </chat-output>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {ref, onMounted} from 'vue';
+import chatOutput from './c-cpns/chat-output.vue';
+const text = ref('');
+
+const _text = "你好，我是本文的作者哈鋆，欢迎加我的微信: ha_kwan";
+let index = 0
+onMounted(() => {
+  let timmer = setInterval(() => {
+    if(index >= _text.length) {
+      clearInterval(timmer)
+      return
+    }
+    text.value += _text[index]
+    index++
+  }, 300); // 光标闪烁的间隔时间（单位：毫秒）
+});
+</script>
 
 <style lang="less" scoped>
 .overview {
-  color: red;
+
 }
 </style>
